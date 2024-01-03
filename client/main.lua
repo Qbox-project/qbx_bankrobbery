b4f901a98a566b822c94e5954e40fe7c1ab8ee7b
Matthew <22198949+MafewTM@users.noreply.github.com>
2024-01-02 20:26:09 -0500

    fix: adjust coords

--- client/main.lua
+++ client/main.lua
@@ -2,9 +2,8 @@ local config = require 'config.client'
 
 function DropFingerprint()
     if IsWearingGloves() then return end
-
-    local coords = GetEntityCoords(cache.ped)
     if config.fingerprintChance > math.random(0, 100) then
+        local coords = GetEntityCoords(cache.ped)
         TriggerServerEvent('evidence:server:CreateFingerDrop', coords)
     end
 end
